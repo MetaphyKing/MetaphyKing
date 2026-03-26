17217837cdc2e6d27fd571c0d321a517e6a67ff2
Randell Logan Smith <logan@metaphysicsandcomputing.com>
2026-03-26 01:59:35 -0700

    Update video links and fallback image in script

--- update_video.py
+++ update_video.py
@@ -3,13 +3,13 @@ import re
 
 # Your 4 videos
 videos = [
-    "https://img1.wsimg.com/blobby/go/bbaabe63-6ba9-4c65-a2ee-d538211cda2a/video/Metaphy%20City.mp4",
-    "https://img1.wsimg.com/blobby/go/bbaabe63-6ba9-4c65-a2ee-d538211cda2a/video/Metaphy%20Circle%20Disk.mp4",
-    "https://img1.wsimg.com/blobby/go/bbaabe63-6ba9-4c65-a2ee-d538211cda2a/video/Raining%20Metaphy.mp4",
-    "https://img1.wsimg.com/blobby/go/bbaabe63-6ba9-4c65-a2ee-d538211cda2a/video/Metaphy%20City%20Skyline.mp4"
+    "https://github.com/user-attachments/assets/0a4ae2e4-9ef1-47d7-bd20-cdf29cd13102",
+    "https://github.com/user-attachments/assets/ff3aada3-63d2-40b8-b7e6-fe436c53879a",
+    "https://github.com/user-attachments/assets/d6263d0a-e7d4-4f13-914a-72ed2459a2a6",
+    "https://github.com/user-attachments/assets/f2f48b68-5341-4c9b-9cfa-cf7c5f63e904"
 ]
 
-fallback_image = "https://img1.wsimg.com/isteam/ip/bbaabe63-6ba9-4c65-a2ee-d538211cda2a/Universal%20Computing%20(2).png/:/rs=w:1160,h:773"
+fallback_image = "https://github.com/user-attachments/assets/113ea173-bc28-4898-ace2-be41e9511e75"
 
 # Pick one at random
 random_video = random.choice(videos)
